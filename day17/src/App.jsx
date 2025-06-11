0cddc2afc1e9bd97ca6930984068be1ad74ea057
SasikalaThennarasu5 <sasikalathennarasu5@gmail.com>
2025-06-11 15:03:18 +0530

    add

--- day17/src/App.jsx
+++ day17/src/App.jsx
@@ -1,67 +1,45 @@
 // App.jsx
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Home from './components/Home';
+import User from './components/User';
+import UserDetails from './components/UserDetails';
+import UserPosts from './components/UserPosts';
+import NotFound from './components/Notfound';
 import Task1 from './components/Task1';
-import Task2 from './components/Task2';
-import Task3 from './components/Task3';
-import Task4 from './components/Task4';
-import Task5 from './components/Task5';
-import Task6 from './components/Task6';
-import Task7 from './components/Task7';
-import Task8 from './components/Task8';
-import Task9 from './components/Task9';
-import Task10 from './components/Task10';
-import Task11 from './components/Task11';
-import Task12 from './components/Task12';
-import Task13 from './components/Task13';
+import Project1 from "./components/Project1";
+import ProductList from "./components/Project2/ProductList";
+import ProductDetails from "./components/Project2/ProductDetails";
+import BlogList from "./components/Project3/BlogList";
+import BlogDetails from "./components/Project3/BlogDetails";
+import MovieList from "./components/Project4/Movielist";
+import MovieDetails from "./components/Project4/MovieDetails";
+import './App.css';
+import Sidebar from "./components/Sidebar";
 
-import Project1 from './components/Project1';
-import Project2 from './components/Project2';
-import Project3 from './components/Project3';
-import Project4 from './components/Project4';
-
-const App = () => {
+function App() {
   return (
-    <Router>
-      <div style={{ padding: "10px" }}>
-        <h1>React Dynamic Routing Tasks</h1>
-        <nav>
-          <ul style={{ listStyle: 'none', paddingLeft: 0 }}>
-            {[...Array(13)].map((_, i) => (
-              <li key={i}><Link to={`/task${i + 1}`}>Task{i + 1}</Link></li>
-            ))}
-            <li><Link to="/user/1">Project1 – User Profile</Link></li>
-            <li><Link to="/product/1">Project2 – Product Details</Link></li>
-            <li><Link to="/post/1">Project3 – Blog App</Link></li>
-            <li><Link to="/movie/1">Project4 – Movie Details</Link></li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/task1" element={<Task1 />} />
-          <Route path="/task2" element={<Task2 />} />
-          <Route path="/task3/:id" element={<Task3 />} />
-          <Route path="/task4/:id" element={<Task4 />} />
-          <Route path="/task5/:id/:name" element={<Task5 />} />
-          <Route path="/task6/:id?" element={<Task6 />} />
-          <Route path="/task7" element={<Task7 />} />
-          <Route path="/task8" element={<Task8 />} />
-          <Route path="/task9/:id?" element={<Task9 />} />
-          <Route path="/task10/:id" element={<Task10 />} />
-          <Route path="/task11/:id/*" element={<Task11 />} />
-          <Route path="/task12/:id" element={<Task12 />} />
-          <Route path="/task13/:id" element={<Task13 />} />
-
-          {/* Mini Projects */}
-          <Route path="/user/:id" element={<Project1 />} />
-          <Route path="/product/:id" element={<Project2 />} />
-          <Route path="/post/:id" element={<Project3 />} />
-          <Route path="/movie/:id" element={<Project4 />} />
-        </Routes>
-      </div>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Sidebar />}>
+          <Route index element={<Home />} />
+          <Route path="task1" element={<Task1 />} />
+          <Route path="user" element={<User />} />
+          <Route path="user/:id" element={<UserDetails />} />
+          <Route path="user/:id/:name" element={<UserDetails />} />
+          <Route path="user/:id/posts" element={<UserPosts />} />
+          <Route path="project1/:id" element={<Project1 />} />
+          <Route path="products" element={<ProductList />} />
+          <Route path="product/:id" element={<ProductDetails />} />
+          <Route path="posts" element={<BlogList />} />
+          <Route path="post/:id" element={<BlogDetails />} />
+          <Route path="movies" element={<MovieList />} />
+          <Route path="movie/:id" element={<MovieDetails />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
--- day17/src/components/Task2.jsx
+++ day17/src/components/Task2.jsx
@@ -1,17 +1,25 @@
 // Task2.jsx
-import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Task2 = () => {
+const Task2 = [
+  { id: "1", name: "Sita", email: "sita@example.com", phone: "123-456-7890"},
+  { id: "2", name: "Rita", email: "rita@example.com", phone: "554-554-3550"},
+  { id: "3", name: "Rita", email: "gita@example.com", phone: "485-256-5552"}
+];
+
+function UserProfile() {
+  const { id } = useParams();
+  const user = users.find((user) => user.id === id);
+  if (!user) return <p>User not found!</p>;
+
   return (
-    <div>
-      <h2>Users</h2>
-      <ul>
-        <li><Link to="/task3/1">User 1</Link></li>
-        <li><Link to="/task3/2">User 2</Link></li>
-      </ul>
+     <div>
+      <h2>User ID: {id}</h2>
+      <p><strong>Name:</strong> {user.name}</p>
+      <p><strong>Email:</strong> {user.email}</p>
+      <p><strong>Phone:</strong> {user.phone}</p>
     </div>
   );
-};
+}
 
 export default Task2;
